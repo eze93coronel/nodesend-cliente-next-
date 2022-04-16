fbd9211dc2d78e4da188d825bae70697be82c7d3
roqueCoronel <rokecoronel748@gmail.com>
2022-04-16 15:44:31 -0300

    creamos el front del pt

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Layout from '../components/Layout'
+
+const Index = () => {
+  return (  
+   <Layout>
+       <h1>index</h1>   
+    </Layout>
+     
+  );
+}
+ 
+export default Index;
