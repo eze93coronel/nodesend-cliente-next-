ee952f579297c99a98a5b1521960afd0cd57fda5
roqueCoronel <rokecoronel748@gmail.com>
2022-04-24 20:45:55 -0300

    modificamos front de index.js se agregaron depenmdencias useDropzone

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import React,{ useContext,useEffect} from 'react';
 import Layout from '../components/Layout'
 import authContext from '../context/auth/authContext'
-
+import Dropzone from '../components/Dropzone';
+import Link from 'next/link';
 const Index = () => {
   //EXTRAEER EL USUARIO AUTENTICADO 
 const AuthContext = useContext(authContext);
@@ -13,7 +14,23 @@ useEffect(() => {
 
   return (  
    <Layout>
-       <h1>index</h1>   
+      <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32"> 
+            <div className="lg:flex md:shadow-lg p-5 bg-white rounded-lg py-10">
+              <Dropzone />
+              <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0"> 
+              <h2 className="text-4xl font-sans font-bold text-gray-800 my-4">compartir aqui archivos de dropson de forma sencilla y privada</h2>
+                <p className="text-lg leading-loose">
+                    <span className="text-red-500 font-bold">ReactNodeSend</span>
+                  
+                </p>
+                <Link href="/crearcuenta">
+                      <a className="text-red-500 font-bold text-lg hover:text-red-700">crea una cuenta para mayores beneficios</a>
+                </Link>
+              </div>
+
+            </div>
+
+      </div>
     </Layout>
      
   );
